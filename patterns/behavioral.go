a144065bf0e05596a2e663b8fa9c9c08fb532503
NuminousBlue <jdiehl2236@gmail.com>
2023-09-10 17:04:48 -0500

    implement interator w/callback, push orientation

--- patterns/behavioral.go
+++ patterns/behavioral.go
@@ -1,1 +1,134 @@
 package main
+
+import "fmt"
+
+func main() {
+	// Observer
+	listener1 := DataListener{
+		Name: "Listener 1",
+	}
+	listener2 := DataListener{
+		Name: "Listener 2",
+	}
+
+	subject := &DataSubject{}
+	subject.register(listener1)
+	subject.register(listener2)
+
+	subject.changeItem("Monday!")
+	subject.changeItem("Tuesday!")
+	subject.changeItem("Wednesday!")
+
+	subject.unregister(listener2)
+
+	subject.changeItem("Wednesday!")
+
+	// Iterator
+	lib.Iterator(printBookName)
+
+	// with anonymous callback
+	lib.Iterator(func(b Book) error {
+		fmt.Println("Book author:", b.Author)
+		return nil
+	})
+}
+
+/*
+OBSERVER
+*/
+type observer interface {
+	onUpdate(data string)
+}
+
+type observable interface {
+	register(obs observer)
+	unregister(obs observer)
+	notifyAll()
+}
+
+type DataSubject struct {
+	observers []DataListener
+	field     string
+}
+type DataListener struct {
+	Name string
+}
+
+func (dl *DataListener) onUpdate(data string) {
+	fmt.Println("Listener", dl.Name, "got data change:", data)
+}
+
+func (ds *DataSubject) changeItem(data string) {
+	ds.field = data
+	ds.notifyAll()
+}
+
+func (ds *DataSubject) register(o DataListener) {
+	ds.observers = append(ds.observers, o)
+}
+
+func (ds *DataSubject) unregister(o DataListener) {
+	var newObservers []DataListener
+
+	for _, obs := range ds.observers {
+		if o.Name != obs.Name {
+			newObservers = append(newObservers, obs)
+		}
+	}
+	ds.observers = newObservers
+}
+
+func (ds *DataSubject) notifyAll() {
+	for _, obs := range ds.observers {
+		obs.onUpdate(ds.field)
+	}
+}
+
+/*
+ITERATOR
+*/
+type Book struct {
+	Name          string
+	Author        string
+	YearPublished int
+}
+
+type Library struct {
+	Collection []Book
+}
+
+var lib *Library = &Library{
+	Collection: []Book{
+		{
+			Name:          "Siddhartha",
+			Author:        "Herman Hesse",
+			YearPublished: 1922,
+		},
+		{
+			Name:          "Flow: The Psychology of Optimal Experience",
+			Author:        "Csikszentmihalyi",
+			YearPublished: 1990,
+		},
+		{
+			Name:          "Man's Search for Meaning",
+			Author:        "Viktor Frankl",
+			YearPublished: 1946,
+		},
+	},
+}
+
+func (l *Library) Iterator(f func(Book) error) {
+	var err error
+
+	for _, b := range l.Collection {
+		err = f(b)
+		if err != nil {
+			fmt.Println("Error encountered while iterating")
+		}
+	}
+}
+
+func printBookName(b Book) error {
+	fmt.Println("Book title:", b.Name)
+	return nil
+}
--- patterns/main.go
+++ patterns/main.go
@@ -1,27 +1,25 @@
 package main
 
-import "fmt"
+// func main() {
 
-func main() {
+// 	// BUILDER PATTERN
+// 	var builderConfig = NewConfigBuilder().
+// 		SetUserId(1).
+// 		SetDomain("localhost").
+// 		SetPort(443).
+// 		SetKey("supersecretkey").
+// 		SetUrl("go/lang/is/awesome").
+// 		Build()
 
-	// BUILDER PATTERN
-	var builderConfig = NewConfigBuilder().
-		SetUserId(1).
-		SetDomain("localhost").
-		SetPort(443).
-		SetKey("supersecretkey").
-		SetUrl("go/lang/is/awesome").
-		Build()
+// 	fmt.Println("Builder Config: ", builderConfig)
 
-	fmt.Println("Builder Config: ", builderConfig)
+// 	// FACTORY PATTERN
+// 	var factoryDevConfig = NewFactoryConfig("dev", 1, "http://localhost:3001", "supersecretdevelopmentkey")
+// 	var factoryProdConfig = NewFactoryConfig("prod", 2, "https://jd-2023.vercel.app", "supersecretproductionkey")
 
-	// FACTORY PATTERN
-	var factoryDevConfig = NewFactoryConfig("dev", 1, "http://localhost:3001", "supersecretdevelopmentkey") 
-	var factoryProdConfig = NewFactoryConfig("prod", 2, "https://jd-2023.vercel.app", "supersecretproductionkey")
+// 	fmt.Println("Factory Dev Config: ", factoryDevConfig)
+// 	fmt.Println("Factory Prod Config: ", factoryProdConfig)
 
-	fmt.Println("Factory Dev Config: ", factoryDevConfig)
-	fmt.Println("Factory Prod Config: ", factoryProdConfig)
-
-	// SINGLETON PATTERN
-	LogConcurrently()
-}
+// 	// SINGLETON PATTERN
+// 	LogConcurrently()
+// }
