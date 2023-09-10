d6f7f325a31928d5ee097c170992ed408c01c78b
NuminousBlue <jdiehl2236@gmail.com>
2023-09-10 16:12:28 -0500

    implement facade pattern

--- patterns/structural.go
+++ patterns/structural.go
@@ -1,30 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /*
 	ADAPTER
 */
 // interface to adapt to
 type remote interface {
-	on() 
+	on()
 	off()
 	volumeUp() int
 	volumeDown() int
 	channelUp() int
 	channelDown() int
 }
+
 // TV implements remote directly
 type TV struct {
-	vol int
+	vol     int
 	channel int
-	isOn bool
+	isOn    bool
 }
-func(tv *TV) on() {
+
+func (tv *TV) on() {
 	fmt.Println("TV on.")
 	tv.isOn = true
 }
-func(tv *TV) off() {
+func (tv *TV) off() {
 	fmt.Println("TV off.")
 	tv.isOn = false
 }
@@ -46,14 +50,16 @@ func (tv *TV) channelUp() int {
 func (tv *TV) channelDown() int {
 	fmt.Println("Channel down.")
 	tv.channel--
-	return tv.channel	
+	return tv.channel
 }
+
 // Radio has the same behaviors but the API is defined differently
 type Radio struct {
 	currentChannel int
-	currentVolume int
-	pluggedIn bool
+	currentVolume  int
+	pluggedIn      bool
 }
+
 func (r *Radio) plugIn() {
 	r.pluggedIn = true
 }
@@ -76,10 +82,12 @@ func (r *Radio) increaseVolume() int {
 	r.currentVolume++
 	return r.currentVolume
 }
-// The adapter has all remote interface methods, which call the corresponding radio methods
+
+// The adapter gets all remote interface methods, which call the corresponding radio methods
 type radioAdapter struct {
 	radio *Radio
 }
+
 func (rd *radioAdapter) on() {
 	rd.radio.plugIn()
 }
@@ -100,5 +108,71 @@ func (rd *radioAdapter) channelDown() int {
 }
 
 /*
-	FACADE
+FACADE
 */
+type MilkKind string
+type Size int
+
+const (
+	WholeMilk   MilkKind = "whole"
+	CoconutMilk MilkKind = "coconut"
+	AlmondMilk  MilkKind = "almond"
+)
+const (
+	Small  Size = 3
+	Medium Size = 5
+	Large  Size = 8
+)
+
+func milkMatcher(kind MilkKind, to string) (MilkKind, error) {
+	if to == "enum" {
+		switch kind {
+		case WholeMilk:
+			return "whole", nil
+		case CoconutMilk:
+			return "coconut", nil
+		case AlmondMilk:
+			return "almond", nil
+		default:
+			return "", fmt.Errorf("failed to get string value of enum")
+		}
+	} else {
+		switch kind {
+		case "whole":
+			return WholeMilk, nil
+		case "coconut":
+			return CoconutMilk, nil
+		case "almond":
+			return AlmondMilk, nil
+		default:
+			return "", fmt.Errorf("failed to get enum value of string")
+		}
+	}
+}
+
+type coffeeOps interface {
+	useBeanVariety(variety string)
+	useDrinkSize(size int)
+	useFoam(units int)
+	grindBeans(amount float32)
+	useHotWater()
+	useColdWater()
+	addSyrup(flavor string)
+	addMilk(kind milkKind)
+}
+
+type coffeeDrinks interface {
+	Drip()
+	Iced()
+	Espresso()
+	CaramelLatte()
+}
+
+type CoffeeMachine struct {
+	operator coffeeOps
+}
+
+func (cm *CoffeeMachine) Drip() {
+	cm.operator.useDrinkSize(Small)
+	cm.operator.grindBeans()
+}
